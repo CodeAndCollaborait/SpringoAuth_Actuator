8b4b03377ec9534412fe88dde1fa8c2faee8b964
meetmmpatel <meetmmpatel@gmail.com>
2020-07-13 14:26:52 -0400

    Added Security Config TokenAuthService

--- src/main/java/com/auth/api/config/WebSecurityConfig.java
+++ src/main/java/com/auth/api/config/WebSecurityConfig.java
@@ -1,19 +1,42 @@
 package com.auth.api.config;
 
-import com.auth.api.filter.JWTAuthFilter;
+import com.auth.api.filter.JWTLogInFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+	BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+	return bCryptPasswordEncoder;
+  }
+  
   
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	
+	String password = "123";
+	String encryptedPassword = this.passwordEncoder().encode(password);
+	System.out.printf("Encoded Password for 123" + encryptedPassword);
+	
+	InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> manageCofig = auth.inMemoryAuthentication();
+	
+	UserDetails userOne = User.withUsername("tom").password(encryptedPassword).roles("USER").build();
+	UserDetails userTwo = User.withUsername("jerry").password(encryptedPassword).roles("USER").build();
+	
+	manageCofig.withUser(userOne);
+	manageCofig.withUser(userTwo);
 	
   }
   
@@ -27,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated()
 			.and()
 			//Add the filter to get Token and authenticate
-			.addFilterBefore(new JWTAuthFilter("/login", authenticationManager()),
+			.addFilterBefore(new JWTLogInFilter("/login", authenticationManager()),
 					UsernamePasswordAuthenticationFilter.class);
 	
 	
--- src/main/java/com/auth/api/filter/JWTLogInFilter.java
+++ src/main/java/com/auth/api/filter/JWTLogInFilter.java
@@ -6,15 +6,16 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class JWTAuthFilter extends AbstractAuthenticationProcessingFilter {
+public class JWTLogInFilter extends AbstractAuthenticationProcessingFilter {
   
   
-  public JWTAuthFilter(String url, AuthenticationManager authenticationManager) {
+  public JWTLogInFilter(String url, AuthenticationManager authenticationManager) {
 	super(new AntPathRequestMatcher(url));
 	setAuthenticationManager(authenticationManager);
   }
@@ -23,10 +24,15 @@ public class JWTAuthFilter extends AbstractAuthenticationProcessingFilter {
   
   
   @Override
-  public Authentication attemptAuthentication(HttpServletRequest httpServletRequest,
-											  HttpServletResponse httpServletResponse) throws AuthenticationException,
-		  IOException, ServletException {
+  public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+		  throws AuthenticationException, IOException, ServletException {
 	return null;
+  }
+  
+  @Override
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain
+		  , Authentication authResult) throws IOException, ServletException {
+	super.successfulAuthentication(request, response, chain, authResult);
   }
   
   
